fae09199abdfc9c091abfba8a7ae4a2108a48c30
Enrique Garcia <EriquzGar10@outlook.com>
2022-04-19 15:32:57 -0500

    Initial Commit

---
+++ SAT.MVC.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAT.MVC.UI.MvcApplication" Language="C#" %>
---
+++ SAT.MVC.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAT.MVC.UI.Startup))]
+namespace SAT.MVC.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
