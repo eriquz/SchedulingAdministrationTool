66bd223a197d3d94f83891e6eee77b7ec70078e8
Enrique Garcia <EriquzGar10@outlook.com>
2022-04-21 15:16:52 -0500

    Functioning Contact form & email confirmation

--- SAT.MVC.UI/Controllers/HomeController.cs
+++ SAT.MVC.UI/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using SAT.MVC.UI.Models;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace SAT.MVC.UI.Controllers
 {
@@ -17,6 +22,52 @@ namespace SAT.MVC.UI.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string msg = $"{cvm.Name} has sent you the following message:<br />" +
+                $"Subject: <strong>{cvm.Subject}</strong><br />" +
+                $"{cvm.Message}" +
+                $"Please reply to {cvm.Email} at your earliest convenience.";
+
+
+            MailMessage mm = new MailMessage(
+
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+                cvm.Subject,
+                msg);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            try
+            {
+                //Send email
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                //Log error in the ViewBag
+                ViewBag.CustomerMessage = $"We're sorry. Your request could not be completed at this time." +
+                    $"Please try again later! <br />" +
+                    $"Error Message:<br />" +
+                    $"{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
---
+++ SAT.MVC.UI/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAT.MVC.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Name is Required*")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Your email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Must provide message content*")]
+        [UIHint("MultilineText")] //Provides a textarea in the UI
+        public string Message { get; set; }
+    }
+}
